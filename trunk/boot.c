2c9ccad6e8ea25cfe465f6b76b3175c1e7962e92
kamil.jiwa@gmail.com <kamil.jiwa@gmail.com@9800275d-640b-eb95-1b8d-e294a41c5e1f>
2012-03-20 10:50:06 +0000

    - fix whitespace - remove .eh_frame from io - use typedefs to make the type more clear in boot.c

--- trunk/boot.c
+++ trunk/boot.c
@@ -1,5 +1,3 @@
-#define _DRIVE_INDEX 0x80
-
 asm (".code16gcc");
 
 asm ("jmp start");
@@ -7,101 +5,104 @@ asm (".space 0x0021");
 asm (".byte 0x80");
 asm (".space 0x001c");
 
-typedef char           int8_t;
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned long  uint32_t;
+typedef char           s8_t;
+typedef unsigned char  u8_t;
+typedef unsigned short u16_t;
+typedef unsigned long  u32_t;
 
 typedef struct
 {
-	int8_t   _a[3];
-	int8_t   name[8];
-	uint16_t bytes_per_sector;
-	uint8_t  sectors_per_cluster;
-	uint16_t reserved_sectors;
-	uint8_t  fats;
-	uint16_t root_entries;
-	uint16_t total_sectors;
-	uint8_t  media_descriptor;
-	uint16_t sectors_per_fat;
-	uint16_t sectors_per_track;
-	uint16_t heads;
-	uint32_t hidden_sectors;
-	uint32_t total_sectors2;
-	uint8_t  drive_index;
-	uint8_t  _b;
-	uint8_t  signature;
-	uint32_t id;
-	int8_t   label[11];
-	int8_t   type[8];
-	uint8_t  _c[448];
-	uint16_t sig;
+	s8_t  _a[3];
+	s8_t  name[8];
+	u16_t bytes_per_sector;
+	u8_t  sectors_per_cluster;
+	u16_t reserved_sectors;
+	u8_t  fats;
+	u16_t root_entries;
+	u16_t total_sectors;
+	u8_t  media_descriptor;
+	u16_t sectors_per_fat;
+	u16_t sectors_per_track;
+	u16_t heads;
+	u32_t hidden_sectors;
+	u32_t total_sectors2;
+	u8_t  drive_index;
+	u8_t  _b;
+	u8_t  signature;
+	u32_t id;
+	s8_t  label[11];
+	s8_t  type[8];
+	u8_t  _c[448];
+	u16_t sig;
 } __attribute__ ((packed)) boot_t;
 
 typedef struct
 {
-	uint8_t  sectors;
-	uint32_t lba;
+	u8_t  sectors;
+	u32_t lba;
 } FILE;
 
 typedef struct
 {
-	int8_t   filename[8];
-	int8_t   extension[3];
-	uint8_t  attributes;
-	uint8_t  _a;
-	uint8_t  create_time_us;
-	uint16_t create_time;
-	uint16_t create_date;
-	uint16_t last_access_date;
-	uint8_t  _b[2];
-	uint16_t last_modified_time;
-	uint16_t last_modified_date;
-	uint16_t cluster;
-	uint32_t size;
+	s8_t  filename[8];
+	s8_t  extension[3];
+	u8_t  attributes;
+	u8_t  _a;
+	u8_t  create_time_us;
+	u16_t create_time;
+	u16_t create_date;
+	u16_t last_access_date;
+	u8_t  _b[2];
+	u16_t last_modified_time;
+	u16_t last_modified_date;
+	u16_t cluster;
+	u32_t size;
 } __attribute__ ((packed)) entry_t;
 
-const uint8_t*      _buffer = (uint8_t*) 0x0500;
-const boot_t* const _bs     = (boot_t*)  0x7c00;
-FILE*               _disk   = (FILE*)    0x7e00;
-uint8_t             _size;
-entry_t*            _entry;
-const int8_t* const _io_bin = "IO      SYS";
+const boot_t* const _bs     = (boot_t*) 0x7c00;
+FILE*               _disk   = (FILE*)   0x7e00;
+const s8_t* const   _io_bin = "IO      SYS";
 
-int8_t
-strncmp()
+u8_t*               _buffer;
+u8_t                _size;
+entry_t*            _entry;
+
+s8_t
+iosyscmp()
 {
-	uint8_t i;
-	for (i = 0; i < 10 && ((int8_t*) _entry)[i] && ((int8_t*) _entry)[i] == _io_bin[i]; ++i);
-	return ((int8_t*) _entry)[i] - _io_bin[i];
+	u8_t i;
+	for (i = 0; i < 10 && ((s8_t*) _entry)[i] && ((s8_t*) _entry)[i] == _io_bin[i]; ++i);
+	return ((s8_t*) _entry)[i] - _io_bin[i];
 }
 
 void
 read()
 {
-	uint32_t t = _bs->heads * _disk->sectors;
-	uint16_t c = _disk->lba / t;
-	uint16_t h = (_disk->lba % t) / _disk->sectors;
+	u32_t t = _bs->heads * _disk->sectors;
+	u16_t c = _disk->lba / t;
+	u16_t h = (_disk->lba % t) / _disk->sectors;
 	c <<= 8;
 	c |= ((_disk->lba % t) % _disk->sectors) + 1;
-	asm ("int $0x0013" : : "a"(0x0200 | _size), "c"(c), "d"((h << 8) | _DRIVE_INDEX), "b"(_buffer));
+	asm ("int $0x13" : : "a"(0x0200 | _size), "c"(c), "d"((h << 8) | 0x80), "b"(_buffer));
 }
 
 void
 start()
 {
-	asm ("int $0x0013" : "=c"(_disk->sectors) : "a"(0x0800), "d"(_DRIVE_INDEX));
+	asm ("int $0x13" : "=c"(_disk->sectors) : "a"(0x0800), "d"(0x80));
 	_disk->sectors &= 0b00111111;
 
+	_buffer = (u8_t*) 0x0500;
 	_disk->lba = _bs->reserved_sectors + (_bs->fats * _bs->sectors_per_fat);
 	_size = _bs->root_entries * sizeof(entry_t) / _bs->bytes_per_sector;
 	read();
 
 	for (_entry = (entry_t*) _buffer; ; ++_entry)
-		if (strncmp() == 0) {
+		if (iosyscmp() == 0) {
+			_buffer = (u8_t*) 0x0700;
 			_disk->lba += _size + (_entry->cluster - 2) * _bs->sectors_per_cluster;
 			_size = 3;
 			read();
-			asm ("jmpl $0x0050, $0x0000");
+			asm ("jmpl $0x0070, $0x0000");
 		}
 }
