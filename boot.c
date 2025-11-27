dc55eb3cd8875e45ae2dc42a95675d43a1d9bdd7
Kamil Jiwa <kamil.jiwa@gmail.com>
2025-11-26 20:50:38 -0800

    Improve naming of variables and functions. Use #define for constants.

--- boot.c
+++ boot.c
@@ -84,7 +84,7 @@ typedef struct
 {
   u8_t  sectors;
   u32_t lba;
-} FILE;
+} disk_t;
 
 /**
  * A root directory entry.
@@ -119,40 +119,48 @@ typedef struct
   u32_t size;
 } __attribute__ ((packed)) entry_t;
 
+#define BOOT_SECTOR_ADDR  0x7c00
+#define DISK_INFO_ADDR    0x7e00
+#define ROOT_DIR_ADDR     0x0500
+#define IO_SYS_LOAD_ADDR  0x0700
+#define FIRST_HDD         0x80
+#define IO_SYS_SECTORS    3
+#define FILENAME_EXT_LEN  11
+
 /**
  * A pointer to the boot sector. After the BIOS POST, the boot sector is loaded
  * into memory at 0x7c00.
  */
-boot_t const* _bs = (boot_t*) 0x7c00;
+boot_t const* boot_sector = (boot_t*) BOOT_SECTOR_ADDR;
 
 /**
  * A pointer to the hard disk drive information. We use 0x7e00, the first block
  * of available memory next to the boot sector.
  */
-FILE* _disk = (FILE*) 0x7e00;
+disk_t* disk_info = (disk_t*) DISK_INFO_ADDR;
 
 /**
  * A constant containing the name of the binary used to initialize the system
  * and device drivers. This is typically IO.SYS or IBMBIO.COM.
  */
-s8_t const* _io_bin = "IO      SYS";
+s8_t const* io_sys_name = "IO      SYS";
 
 /**
  * A pointer to a general-purpose buffer in memory. Used to store the root
  * directory entries and the first 3 sectors of IO.SYS.
  */
-u8_t* _buffer;
+u8_t* buffer;
 
 /**
- * A variable used by the read() method to indicate how many sectors to read
- * from the hard disk into memory.
+ * A variable used by the read_sectors() method to indicate how many sectors to
+ * read from the hard disk into memory.
  */
-u8_t _size;
+u8_t sector_count;
 
 /**
  * A pointer to the root directory entry currently being read.
  */
-entry_t const* _entry;
+entry_t const* current_entry;
 
 /**
  * Checks if the current root directory entry has the name IO.SYS by performing
@@ -162,33 +170,34 @@ entry_t const* _entry;
  *     value if the entry name is greater than IO.SYS.
  */
 s8_t
-iosyscmp()
+is_io_sys(void)
 {
   u16_t i;
   for (i = 0;
-       i < 10 && ((s8_t*) _entry)[i] && ((s8_t*) _entry)[i] == _io_bin[i];
+       i < FILENAME_EXT_LEN - 1 && 
+       ((s8_t*) current_entry)[i] && 
+       ((s8_t*) current_entry)[i] == io_sys_name[i];
        ++i);
-  return ((s8_t*) _entry)[i] - _io_bin[i];
+  return ((s8_t*) current_entry)[i] - io_sys_name[i];
 }
 
 /**
  * Reads sectors at the provided LBA from the hard disk into memory. Uses the
- * value stored in _size as the number of sectors to read.
+ * value stored in sector_count as the number of sectors to read.
  * @return 0
  */
-u16_t
-read()
+void
+read_sectors(void)
 {
   // convert the LBA into CHS values
-  u32_t t = _bs->heads * _disk->sectors;
-  u16_t c = _disk->lba / t;
-  u16_t h = (_disk->lba % t) / _disk->sectors;
-  c <<= 8;
-  c |= ((_disk->lba % t) % _disk->sectors) + 1;
+  u32_t sectors_per_cylinder = boot_sector->heads * disk_info->sectors;
+  u16_t cylinder = disk_info->lba / sectors_per_cylinder;
+  u16_t head = (disk_info->lba % sectors_per_cylinder) / disk_info->sectors;
+  cylinder <<= 8;
+  cylinder |= ((disk_info->lba % sectors_per_cylinder) % disk_info->sectors) + 1;
 
-  // read sectors from the hard disk into memory at 0x0500
-  asm ("int $0x13" : : "a"(0x0200 | _size), "b"(_buffer), "c"(c), "d"((h << 8) | 0x0080));
-  return 0;
+  // read sectors from the hard disk into memory
+  asm ("int $0x13" : : "a"(0x0200 | sector_count), "b"(buffer), "c"(cylinder), "d"((head << 8) | FIRST_HDD));
 }
 
 /**
@@ -197,29 +206,29 @@ read()
  * @return 0
  */
 u16_t
-_start()
+_start(void)
 {
-  // read hard disk drive information into memory at 0x7e00
-  asm ("int $0x13" : "=c"(_disk->sectors) : "a"(0x0800), "d"(0x80) : "bx");
-  _disk->sectors &= 0b00111111;
+  // read hard disk drive information into memory
+  asm ("int $0x13" : "=c"(disk_info->sectors) : "a"(0x0800), "d"(FIRST_HDD) : "bx");
+  disk_info->sectors &= 0b00111111;
 
-  // read the root directory into memory at 0x500
-  _buffer = (u8_t*) 0x0500;
-  _disk->lba = _bs->reserved_sectors + (_bs->fats * _bs->sectors_per_fat);
-  _size = _bs->root_entries * sizeof(entry_t) / _bs->bytes_per_sector;
-  read();
+  // read the root directory into memory
+  buffer = (u8_t*) ROOT_DIR_ADDR;
+  disk_info->lba = boot_sector->reserved_sectors + (boot_sector->fats * boot_sector->sectors_per_fat);
+  sector_count = boot_sector->root_entries * sizeof(entry_t) / boot_sector->bytes_per_sector;
+  read_sectors();
 
   // iterate over root directory entries and look for IO.SYS
-  for (_entry = (entry_t*) _buffer; ; ++_entry)
-    if (iosyscmp() == 0) {
-      // load the first 3 sectors of IO.SYS into memory at 0x0700
-      _buffer = (u8_t*) 0x0700;
-      _disk->lba += _size + (_entry->cluster - 2) * _bs->sectors_per_cluster;
-      _size = 3;
-      read();
+  for (current_entry = (entry_t*) buffer; ; ++current_entry)
+    if (is_io_sys() == 0) {
+      // load the first 3 sectors of IO.SYS into memory
+      buffer = (u8_t*) IO_SYS_LOAD_ADDR;
+      disk_info->lba += sector_count + (current_entry->cluster - 2) * boot_sector->sectors_per_cluster;
+      sector_count = IO_SYS_SECTORS;
+      read_sectors();
 
       // execute IO.SYS
-      asm ("jmpw %0, %1" : : "g"(0x0000), "g"(0x0700));
+      asm ("jmpw %0, %1" : : "g"(0x0000), "g"(IO_SYS_LOAD_ADDR));
     }
 
   return 0;
